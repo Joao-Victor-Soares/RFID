790793318c99a320b1f98782a6bbcfeac58a3e69
João Victor <78230200+Joao-Victor-Soares@users.noreply.github.com>
2024-08-20 12:28:49 -0300

    Update WS_rfid_online.py

--- WS_rfid_online.py
+++ WS_rfid_online.py
@@ -116,5 +116,5 @@ def conexao():
     return jsonify({'status': 'conectado'}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
